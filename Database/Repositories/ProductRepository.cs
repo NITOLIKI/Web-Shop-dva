49bc96a7bc2ff20fa54c8b92e6ebd7a55ebd7178
NIKOLIKI <lukanikolic752@gmail.com>
2023-01-18 14:53:15 +0100

    znam kako ovo radi

---
+++ Database/Repositories/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Domain;
+using Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+        public void Insert(Proizvod product)
+        {
+
+        }
+    }
+}
